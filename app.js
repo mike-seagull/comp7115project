9249ffa4afe564340a9e1fa3e0d5c5e626f9ae1f
Michael Hollister <hollister.michael@gmail.com>
2016-09-28 09:15:04 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function(req, res) {
 	res.send("Works");
-}
+});
 
-app.listen(3000, function() {
-	console.log("Running on port 3000");
+app.listen(app.get('port'), function() {
+	console.log("Running on port ", app.get('port'));
 });
