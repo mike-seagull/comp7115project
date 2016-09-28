8d694b0cc42edc9d02ecb5bc50427f5938baf90c
Michael Hollister <hollister.michael@gmail.com>
2016-09-28 17:46:04 -0500

    added db app variables

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var app = express();
 
+// app variables
 app.set('port', (process.env.PORT || 3000));
+app.set('MYSQL_HOSTNAME', 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+app.set('MYSQL_USERNAME', 'a0i3kvl4stvs4dvh');
+app.set('MYSQL_PASSWORD', 'vyrzttz9k2q9na2r');
 
+app.set('NEO4J_USERNAME', 'app57210483-jgyI3b');
+app.set('NEO4J_PASSWORD', '7HpPCWui68YzQRMgfjuG');
+app.set('NEO4J_URL', 'http://'+app.get('NEO4J_USERNAME')+':'+app.get('NEO4J_PASSWORD')+'@hobby-fplmbomjfhocgbkelakdbenl.dbs.graphenedb.com:24789');
+
+// app routes
 app.get('/', function(req, res) {
 	res.send("Works");
 });
 
+
+// start server
 app.listen(app.get('port'), function() {
 	console.log("Running on port ", app.get('port'));
 });
+
